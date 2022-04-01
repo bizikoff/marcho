175d7c236e9a16f0e7ba378c0e7da7e668c3cb4d
bizikoff <bizikovvlad@gmail.com>
2022-04-01 18:57:26 +0300

    added product section

--- app/js/main.js
+++ app/js/main.js
@@ -5,4 +5,13 @@ $(function() {
     fade: true,
     autoplay: true
   });
+
+  $(".star").rateYo({ 
+    starWidth: "18px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+    readOnly: true,
+  });
 })
+
+  
